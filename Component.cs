05ced8bf7a138d5d0d59d8674c75318510d4751e
a.amraei <amin.amraei@gmail.com>
2023-12-30 16:21:34 +0330

    Add project files.

---
+++ Component.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public abstract class Component
+    {
+        public Component()
+        {
+            
+        }
+        public abstract string Operation();
+        public virtual void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        // You can provide a method that lets the client code figure out whether
+        // a component can bear children.
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+    }
+}
